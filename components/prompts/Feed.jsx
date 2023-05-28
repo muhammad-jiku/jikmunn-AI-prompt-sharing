cb3fa225a0fb52f378f76ce4b0c8809a2f853b5b
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-28 14:51:23 +0600

    some modifications for home page

--- components/prompts/Feed.jsx
+++ components/prompts/Feed.jsx
@@ -38,22 +38,33 @@ const Feed = () => {
 	}, []);
 
 	return (
-		<section className='feed'>
-			<form className='relative w-full flex-center'>
-				<input
-					type='text'
-					placeholder='Search for a tag or a username'
-					value={searchText}
-					onChange={handleSearchChange}
-					required
-					className='search_input peer'
-				/>
-			</form>
+		<section className='w-full flex-center flex-col'>
+			<h1 className='head_text text-center'>
+				Discover & Share
+				<br className='max-md:hidden' />
+				<span className='orange_gradient text-center'> AI-Powered Prompts</span>
+			</h1>
+			<p className='desc text-center'>
+				Promptopia is an open-source AI prompting tool for modern world to
+				discover, create and share creative prompts
+			</p>
+			<section className='feed'>
+				<form className='relative w-full flex-center'>
+					<input
+						type='text'
+						placeholder='Search for a tag or a username'
+						value={searchText}
+						onChange={handleSearchChange}
+						required
+						className='search_input peer'
+					/>
+				</form>
 
-			<PromptCardList
-				data={posts}
-				handleTagClick={() => {}}
-			/>
+				<PromptCardList
+					data={posts}
+					handleTagClick={() => {}}
+				/>
+			</section>
 		</section>
 	);
 };
