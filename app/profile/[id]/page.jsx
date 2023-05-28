f3882baeee9f97f247e060ca3523c898eafe1e56
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-28 20:58:28 +0600

    user profile page

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,6 @@
+import { UserProfile } from '@/components';
+import React from 'react';
+
+export default function UserProfilePage() {
+	return <UserProfile />;
+}
--- components/index.js
+++ components/index.js
@@ -2,3 +2,4 @@ export { default as Feed } from './prompts/Feed';
 export { default as CreatePrompt } from './prompts/CreatePrompt';
 export { default as UpdatePrompt } from './prompts/UpdatePrompt';
 export { default as MyProfile } from './profile/MyProfile';
+export { default as UserProfile } from './profile/UserProfile';
