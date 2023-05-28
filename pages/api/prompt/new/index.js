b5328cd2d9c52cc57462a1479be60eb6e45d8c3e
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-28 21:40:34 +0600

    api new prompt route

---
+++ pages/api/prompt/new/index.js
@@ -0,0 +1,20 @@
+import Prompt from '@/backend/models/prompt';
+import { connectToDB } from '@/backend/utils/database';
+
+export const POST = async (request) => {
+	const { userId, prompt, tag } = await request.json();
+
+	try {
+		await connectToDB();
+		const newPrompt = new Prompt({ creator: userId, prompt, tag });
+
+		await newPrompt.save();
+		return new Response(JSON.stringify(newPrompt), {
+			status: 201,
+		});
+	} catch (error) {
+		return new Response('Failed to create a new prompt', {
+			status: 500,
+		});
+	}
+};
