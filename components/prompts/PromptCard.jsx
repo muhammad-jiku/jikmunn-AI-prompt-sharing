167a2615887cef3a098f42c18a0cd816eb8a0414
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-28 19:31:29 +0600

    prompt card component modification

--- components/prompts/PromptCard.jsx
+++ components/prompts/PromptCard.jsx
@@ -6,22 +6,33 @@ import { useSession } from 'next-auth/react';
 import { usePathname, useRouter } from 'next/navigation';
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+	const [copied, setCopied] = useState('');
+
 	const { data: session } = useSession();
 	const pathName = usePathname();
 	const router = useRouter();
 
-	const [copied, setCopied] = useState('');
+	const handleProfileClick = () => {
+		console.log(post);
+
+		if (post.creator._id === session?.user.id) return router.push('/profile');
+
+		router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+	};
 
 	const handleCopy = () => {
 		setCopied(post.prompt);
 		navigator.clipboard.writeText(post.prompt);
-		setTimeout(() => setCopied(''), 3000);
+		setTimeout(() => setCopied(false), 3000);
 	};
 
 	return (
 		<div className='prompt_card'>
 			<div className='flex justify-between items-start gap-5'>
-				<div className='flex-1 flex justify-start items-center gap-3 cursor-pointer'>
+				<div
+					className='flex-1 flex justify-start items-center gap-3 cursor-pointer'
+					onClick={handleProfileClick}
+				>
 					<Image
 						src={post.creator.image}
 						alt='user_image'
@@ -50,6 +61,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 								? '/assets/icons/tick.svg'
 								: '/assets/icons/copy.svg'
 						}
+						alt={copied === post.prompt ? 'tick_icon' : 'copy_icon'}
 						width={12}
 						height={12}
 					/>
