209d116590a69ec38f75ea8aa83db70332f89eec
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-28 21:50:48 +0600

    update single prompt api route

--- pages/api/prompt/[id]/index.js
+++ pages/api/prompt/[id]/index.js
@@ -20,3 +20,28 @@ export const GET = async (request, { params }) => {
 		});
 	}
 };
+
+export const PATCH = async (request, { params }) => {
+	const { prompt, tag } = await request.json();
+
+	try {
+		await connectToDB();
+
+		// Find the existing prompt by ID
+		const existingPrompt = await Prompt.findById(params.id);
+
+		if (!existingPrompt) {
+			return new Response('Prompt not found', { status: 404 });
+		}
+
+		// Update the prompt with new data
+		existingPrompt.prompt = prompt;
+		existingPrompt.tag = tag;
+
+		await existingPrompt.save();
+
+		return new Response('Successfully updated the Prompts', { status: 200 });
+	} catch (error) {
+		return new Response('Error Updating Prompt', { status: 500 });
+	}
+};
