68e139fd372d13188e8c0810045061129b194d6e
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-29 12:11:42 +0600

    auth pages

--- components/index.js
+++ components/index.js
@@ -1,4 +1,6 @@
 export { default as Feed } from './prompts/Feed';
+export { default as SignUp } from './auth/SignUp';
+export { default as SignIn } from './auth/SignIn';
 export { default as CreatePrompt } from './prompts/CreatePrompt';
 export { default as UpdatePrompt } from './prompts/UpdatePrompt';
 export { default as MyProfile } from './profile/MyProfile';
