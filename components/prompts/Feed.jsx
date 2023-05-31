8e43ddf02a74800e3b119b483a08784c236b5df3
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-31 13:14:10 +0600

    display prompts

--- components/prompts/Feed.jsx
+++ components/prompts/Feed.jsx
@@ -27,7 +27,7 @@ const Feed = () => {
 		const response = await fetch('/api/prompt');
 		const data = await response.json();
 
-		setAllPosts(data);
+		setAllPosts(data?.data);
 	};
 
 	useEffect(() => {
--- pages/api/prompt/index.js
+++ pages/api/prompt/index.js
@@ -1,18 +1,30 @@
 import Prompt from '@/backend/models/prompt';
 import { connectToDB } from '@/backend/utils/database';
 
-export const GET = async (request) => {
-	try {
-		await connectToDB();
+export default async function handler(req, res) {
+	if (req.method === 'GET') {
+		// Process a POST request
+		try {
+			await connectToDB();
+			const prompts = await Prompt.find({}).populate('creator');
 
-		const prompts = await Prompt.find({}).populate('creator');
+			console.log('prompts', prompts);
 
-		return new Response(JSON.stringify(prompts), {
-			status: 200,
-		});
-	} catch (error) {
-		return new Response('Failed to fetch all prompts', {
-			status: 500,
+			res.status(200).json({
+				success: true,
+				message: 'All the prompts!',
+				data: prompts,
+			});
+		} catch (error) {
+			console.log(error);
+			return res.status(500).json({
+				success: false,
+				message: 'Something went wrong',
+			});
+		}
+	} else {
+		return res.status(500).json({
+			message: 'Something went wrong',
 		});
 	}
-};
+}
