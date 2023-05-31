55c076ddc33914bb3af6f1249caedfdf9337e901
muhammad-jiku <muhammadjiku364@gmail.com>
2023-05-31 16:34:06 +0600

    user profile api

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,8 +1,20 @@
+'use client';
+
 import { UserProfile } from '@/components';
+import { useSearchParams } from 'next/navigation';
 import React from 'react';
 
-const Page = () => {
-	return <UserProfile />;
+const Page = ({ params }) => {
+	const searchParams = useSearchParams();
+	const userName = searchParams.get('name');
+	const { id } = params;
+
+	return (
+		<UserProfile
+			id={id}
+			userName={userName}
+		/>
+	);
 };
 
 export default Page;
--- components/profile/UserProfile.jsx
+++ components/profile/UserProfile.jsx
@@ -1,25 +1,21 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useSearchParams } from 'next/navigation';
 import Profile from './Profile';
 
-const UserProfile = ({ params }) => {
-	const searchParams = useSearchParams();
-	const userName = searchParams.get('name');
-
+const UserProfile = ({ id, userName }) => {
 	const [userPosts, setUserPosts] = useState([]);
 
 	useEffect(() => {
 		const fetchPosts = async () => {
-			const response = await fetch(`/api/users/${params?.id}/posts`);
+			const response = await fetch(`/api/users/${id}/posts`);
 			const data = await response.json();
 
-			setUserPosts(data);
+			setUserPosts(data?.data);
 		};
 
-		if (params?.id) fetchPosts();
-	}, [params.id]);
+		if (id) fetchPosts();
+	}, [id]);
 
 	return (
 		<Profile
